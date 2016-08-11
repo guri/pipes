371d7aad5772bcb207d225cda9fa62c9cfbb8f4e
guri <makes.reality@gmail.com>
2016-08-11 22:20:59 +0300

    created new pipe - radians. it will convert degrees to radians

--- src/pipes/radians.pipe.ts
+++ src/pipes/radians.pipe.ts
@@ -1,3 +1,30 @@
 /**
  * Created by guri on 11/08/16.
  */
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+
+@Pipe({
+    name:'radians'
+})
+
+export class Radians implements PipeTransform {
+
+    isNumber(value:any):boolean {
+        return typeof value === 'number';
+    }
+
+    transform(degrees: any, decimal = 2:any) : any {
+
+        // if decimal is not an integer greater than -1, we cannot do. quit with error "NaN"
+        // if degrees is not a real number, we cannot do also. quit with error "NaN"
+        if(this.isNumber(decimal) && isFinite(decimal) && decimal%1===0 && decimal >= 0 &&
+            this.isNumber(degrees) && isFinite(degrees)) {
+            var radians = (degrees * Math.PI) / 180;
+            return Math.round(radians * Math.pow(10,decimal)) / (Math.pow(10,decimal));
+        }
+        return "NaN";
+
+    }
+}
